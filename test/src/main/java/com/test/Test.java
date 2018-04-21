9007650cb2c1c31f144ddd8dba2c19ca312bb064
wuql <v-qilin.wu@oneplus.net>
2018-04-21 15:05:15 +0800

    test

---
+++ test/src/main/java/com/test/Test.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * Copyright (C) 2013 - 2018 ShenZhen OnePlus Technology Co.,Ltd
+ * All Rights Reserved.
+ * 本软件为深圳万普拉斯科技有限公司开发研制。未经本公司正式书面同意，其他任何个人、团体不得使用、复制、修改或发布本软件.
+ *****************************************************************************/
+package com.test;
+
+/**
+ * @author kylin
+ * @version V1.0
+ * @Title: Test.java
+ * @Package com.test
+ * @Description test
+ * @date 2018 04-21 15:04.
+ */
+public class Test {
+
+    public static void main(String[] atrs){
+        System.out.print("1");
+    }
+}
